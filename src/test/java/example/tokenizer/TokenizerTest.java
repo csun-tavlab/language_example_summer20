75110cd8e9a4a728cb52031621fb80457080cd65
Kyle Dewey <kyle.dewey@csun.edu>
2020-06-08 11:11:31 -0700

    cleaned up tests and added a test with whitespace; tests currently failing

--- src/test/java/example/tokenizer/TokenizerTest.java
+++ src/test/java/example/tokenizer/TokenizerTest.java
@@ -1,24 +1,50 @@
 package example.tokenizer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 
 public class TokenizerTest {
+    public static void assertTokenizes(final String input,
+                                       final Token[] expectedTokens)
+        throws TokenizerException {
+        final Tokenizer tokenizer = new Tokenizer(input);
+        final Token[] receivedTokens = tokenizer.tokenize();
+        assertArrayEquals(expectedTokens, receivedTokens);
+    }
+    
     // "" = []
     @Test
     public void testEmpty() throws TokenizerException {
-        final Tokenizer tokenizer = new Tokenizer("");
-        final Token[] tokens = tokenizer.tokenize();
-        // assertEquals(expectedValue, givenValue)
-        assertEquals(0, tokens.length);
+        assertTokenizes("", new Token[0]);
     }
 
     // '123' = [123]
     @Test
     public void testSingleIntegerToken() throws TokenizerException {
-        final Tokenizer tokenizer = new Tokenizer("123");
-        final Token[] tokens = tokenizer.tokenize();
-        assertEquals(1, tokens.length);
-        assertEquals(new IntegerToken(123), tokens[0]);
+        assertTokenizes("123", new Token[]{ new IntegerToken(123) });
+    }
+
+    // '+' = [+]
+    @Test
+    public void testPlus() throws TokenizerException {
+        assertTokenizes("+", new Token[]{ new PlusToken() });
+    }
+
+    // '-' = [-]
+    @Test
+    public void testMinus() throws TokenizerException {
+        assertTokenizes("-", new Token[]{ new MinusToken() });
+    }
+
+    // '*' = [*]
+    @Test
+    public void testMultiply() throws TokenizerException {
+        assertTokenizes("*", new Token[]{ new MultiplyToken() });
+    }
+
+    // ' ' = []
+    @Test
+    public void testWhitespace() throws TokenizerException {
+        assertTokenizes(" ", new Token[0]);
     }
 }
